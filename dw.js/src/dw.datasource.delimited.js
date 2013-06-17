6a10cef87cdb35789c9176af3848b1bde468b805
Gregor Aisch <contact@vis4.net>
2013-06-17 23:51:25 +0200

    fixed delimited parser

--- dw.js/src/dw.datasource.delimited.js
+++ dw.js/src/dw.datasource.delimited.js
@@ -45,7 +45,7 @@ dw.datasource.delimited = function(opts) {
 
 var DelimitedParser = function(opts) {
 
-    opts = _.extend(opts, {
+    opts = _.extend({
         delimiter: "auto",
         quoteChar: "\"",
         skipRows: 0,
@@ -53,7 +53,7 @@ var DelimitedParser = function(opts) {
         transpose: false,
         firstRowIsHeader: true,
         firstColumnIsHeader: true
-    });
+    }, opts);
 
     this.__delimiterPatterns = getDelimiterPatterns(opts.delimiter, opts.quoteChar);
     this.opts = opts;
